c6a5003a1bc0509fb4be421868fd13e085264919
Raphael Alves <rapha.alves75@gmail.com>
2023-09-17 20:38:51 -0300

    Atualização com as sugestões de feedbacks recebidas: -> Tratamento de erro e exceções -> Delimitador com virgula -> Leitura a partir de um arquivo

--- csvordergenerator.py
+++ csvordergenerator.py
@@ -2,54 +2,82 @@ import requests
 import json
 import csv
 
-def entityorder(listorders):
-#Criacao do arquivo csv e escrita do cabecalho        
-    filecsv = open('csvimport.csv', 'w', newline='') 
-    fieldnames = "ID Ordem", "ID Item", "Descrição do produto", "ID Envio", "Estado", "Subestado", "Tipo de logistica","Destino do envio","ID Agência" ,"ID Carrier","Endereço do Receptor"
-    wrt = csv.writer(filecsv, delimiter=',')
-    wrt.writerow(fieldnames)    
-    separator = ' '
-    
-#Inicio entity orders
-    for i in range(len(listorders)):
-        requestorders = requests.get('https://6f008c57-99e0-4a2e-8d80-782a71cf99db.mock.pstmn.io/orders/'+str(listorders[i]))
-        orders = json.loads(requestorders.content)
-        idorder = str(orders['id'])
-        shipmentid = str(orders['shipping']['id'])
-        
-        for orderitems in orders['order_items']:
-            iditem = orderitems['item']['id']
-            nomeitem = orderitems['item']['title']
-            if orderitems['item']['variation_attributes'] is not None:
-                for attributes in orderitems['item']['variation_attributes']:
-                    coratt = attributes['name']+" "+attributes['value_name']
-            else:
-                coratt = "" 
-                
-#Inicio entity shipment                                               
-        requestshipment = requests.get('https://6f008c57-99e0-4a2e-8d80-782a71cf99db.mock.pstmn.io/shipments/'+str(shipmentid))
-        shipments = json.loads(requestshipment.content)
-        status = shipments['status']
-        substatus = shipments['substatus']
-        logistictype = shipments['logistic_type']
-        receiver =  shipments['receiver_address']['street_name'], shipments['receiver_address']['street_number'], shipments['receiver_address']['city']['name'], shipments['receiver_address']['zip_code']
-        
-        if shipments['receiver_address']['agency'] is not None:
-            recipient = "Agência"
-            agencyid = shipments['receiver_address']['agency']['agency_id']
-            carrierid = shipments['receiver_address']['agency']['carrier_id']
+
+def read_file(file):
+    try:
+        open_file = open(file, 'r')
+        read_orders = open_file.read().split()
+        if len(read_orders) == 0:
+            return None
         else:
-            recipient = "Domicilio"
-            agencyid = ""
-            carrierid = ""
-            
-#Escrita no CSV                
-        csvouput = idorder, iditem, nomeitem+", "+coratt, shipmentid, status, substatus, logistictype, recipient, agencyid, carrierid, separator.join(receiver)
-        wrt.writerow(csvouput)
-#Fechar arquivo        
-    filecsv.close()     
-
-#chamada da funcao
-entityorder([4114988927,4114988960,4114999549])
+            return read_orders
+    except FileNotFoundError as e:
+        print(f"Arquivo não encontrado \n"
+              f"{e}")
+        return None
 
 
+def entityorder(order):
+    if order is not None:
+        # Criacao do arquivo csv e escrita do cabecalho
+        filecsv = open('csvimport.csv', 'w', newline='')
+        fieldnames = "ID Ordem", "ID Item", "Descrição do produto", "ID Envio", "Estado", "Subestado", "Tipo de logistica", "Destino do envio", "ID Agência", "ID Carrier", "Endereço do Receptor"
+        wrt = csv.writer(filecsv, delimiter=',')
+        wrt.writerow(fieldnames)
+        separator = ' '
+
+        # Inicio entity orders
+        for i in range(len(order)):
+            requestorders = requests.get(
+                'https://6f008c57-99e0-4a2e-8d80-782a71cf99db.mock.pstmn.io/orders/' + str(order[i]))
+            orders = json.loads(requestorders.content)
+            try:
+                idorder = str(orders['id'])
+                shipmentid = str(orders['shipping']['id'])
+
+                for orderitems in orders['order_items']:
+                    iditem = orderitems['item']['id']
+                    nomeitem = orderitems['item']['title']
+                    if orderitems['item']['variation_attributes'] is not None:
+                        for attributes in orderitems['item']['variation_attributes']:
+                            coratt = attributes['name'] + " " + attributes['value_name']
+                    else:
+                        coratt = ""
+
+                # Inicio entity shipment
+                requestshipment = requests.get(
+                    'https://6f008c57-99e0-4a2e-8d80-782a71cf99db.mock.pstmn.io/shipments/' + str(shipmentid))
+                shipments = json.loads(requestshipment.content)
+                status = shipments['status']
+                substatus = shipments['substatus']
+                logistictype = shipments['logistic_type']
+                receiver = shipments['receiver_address']['street_name'], shipments['receiver_address']['street_number'], \
+                    shipments['receiver_address']['city']['name'], shipments['receiver_address']['zip_code']
+
+                if shipments['receiver_address']['agency'] is not None:
+                    recipient = "Agência"
+                    agencyid = shipments['receiver_address']['agency']['agency_id']
+                    carrierid = shipments['receiver_address']['agency']['carrier_id']
+                else:
+                    recipient = "Domicilio"
+                    agencyid = ""
+                    carrierid = ""
+
+                # Escrita no CSV
+                csvouput = idorder, iditem, nomeitem + ", " + coratt, shipmentid, status, substatus, logistictype, recipient, agencyid, carrierid, separator.join(
+                    receiver)
+                wrt.writerow(csvouput)
+            except KeyError as e:
+                print(f"Não foi possivel retornar informações para o dado "f"{dados[i]}"" pois o parametro "f"{e} não "
+                      f"existe \n"
+                      f"{orders}")
+
+        # Fechar arquivo
+        return filecsv.close()
+
+    else:
+        print("CSV não gerado, pois os dados estão vazios")
+
+
+# chamada da funcao
+entityorder(read_file('teste.txt'))
